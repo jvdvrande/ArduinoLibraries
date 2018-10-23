358120d64e3e2f73d6e472e08dfd76f567dec2c1
Joris <jvdvrande@hotmail.com>
2018-10-23 22:47:53 +0200

    Fixes Sonar Distance calculations
    Fixed time_to_distance constants
    Fixes average calculations with invalid distances

--- Sonar/HC_SR04.cpp
+++ Sonar/HC_SR04.cpp
@@ -3,19 +3,14 @@
 #define SIGNAL_TIME_US  2
 #define TRIGGER_TIME_US 10
 
-//
 #define ECHO_TIMEOUT    25000L	// 25ms = 2 * 4.2875m
 //#define ECHO_TIMEOUT    150000L	// 150ms
 
 HC_SR04::HC_SR04(uint8_t trigger_pin, uint8_t echo_pin)
+    : m_trigger_pin(trigger_pin)
+    , m_echo_pin(echo_pin)
 {
-    m_trigger_pin = trigger_pin;
-    m_echo_pin = echo_pin;
-
-    init();
 }
-
-// ----------------------------------------------
 
 void HC_SR04::init()
 {
@@ -23,6 +18,44 @@ void HC_SR04::init()
     pinMode(m_echo_pin,    INPUT);
 }
 
+// ----------------------------------------------
+float HC_SR04::distance_avg(uint8_t wait, uint8_t count) const
+{
+    float min_distance = 9.f;
+    float max_distance = 0.f;
+    float average_sum = 0.f;
+    uint8_t average_count = 0;
+    float current_distance;
+    
+    // minimal 1 sample
+    count = max(1, count);
+
+    for (int i = 0; i < count + 2; ++i)
+    {
+        current_distance = distance();
+        if (current_distance == 0.f)
+            continue;
+
+        // update limits
+        min_distance = min(min_distance, current_distance);
+        max_distance = max(max_distance, current_distance);
+
+        // update sum
+        average_sum += current_distance;
+        ++average_count;
+
+        // wait for specified interval
+        delay(wait);
+    }
+
+    // calculate average (ignoring min/max values)
+    if (average_count > 2)
+        average_sum = (average_sum - (max_distance + min_distance)) / (average_count - 2);
+    
+    return average_sum;
+}
+
+// ----------------------------------------------
 long HC_SR04::ping() const
 {
     // make sure trigger_pin is LOW
@@ -41,38 +74,15 @@ long HC_SR04::ping() const
     return pulseIn(m_echo_pin, HIGH, ECHO_TIMEOUT);
 }
     
-float HC_SR04::distance_avg(uint8_t wait, uint8_t count) const
+float HC_SR04::response_time_to_distance(long echo_response_us) const
 {
-    float min_distance = 9.f;
-    float max_distance = 0.f;
-    float average_sum = 0.f;
-    float current_distance;
-    
-    // minimal 1 sample
-    count = max(1, count);
-
-    for (int i = 0; i < count + 2; ++i)
-    {
-        current_distance = distance();
-
-        // update limits
-        min_distance = min(min_distance, current_distance);
-        max_distance = max(max_distance, current_distance);
-
-        // update sum
-        average_sum += current_distance;
-        
-        // wait for specified interval
-        delay(wait);
-    }
-
-    // calculate average (ignoring min/max values)
-    return (average_sum - (max_distance + min_distance)) / count;
+    //*
+    return (echo_response_us * RESPONE_US_TO_METER);
+    /*/
+    return (echo_response_us / RESPONE_US_TO_METER_INV);
+    //*/
 }
-
-// ----------------------------------------------
-
-
+	
 
 
 
--- Sonar/HC_SR04.h
+++ Sonar/HC_SR04.h
@@ -6,30 +6,29 @@
 // 343 m/s
 #define SOUND_SPEED 343.f
 
-// 0.000686f m/us (response time is two-way, sound travels the distance twice)
-#define RESPONE_US_TO_METER 0.000686f
+// 0.000343 m/us (response time is two-way, sound travels the distance twice)
+#define RESPONE_US_TO_METER     0.0001715f
 // 1.f/RESPONE_US_TO_METER to overcome float-precision errors
-#define RESPONE_US_TO_METER_INV 2915.45f
+#define RESPONE_US_TO_METER_INV 5830.90f
 
 class HC_SR04
 {
 public:
 	HC_SR04(uint8_t trigger_pin, uint8_t echo_pin);
 
+    void init();
+    
 public:
 	float distance()            const {return response_time_to_distance(ping());}
 	float distance_avg(uint8_t wait = 2, uint8_t count = 5) const;
 
 private:
-    void init();
-
     // returns echo-response time in microseconds 
     long ping() const;
 
 	// Do the measurement calculation and return distance in meter
-	float response_time_to_distance(long echo_response_us)        const {return (echo_response_us * RESPONE_US_TO_METER);}
-    // float response_time_to_distance(long echo_response_us)     const   {return (echo_response_us / RESPONE_US_TO_METER_INV);}
-	
+	float response_time_to_distance(long echo_response_us)        const;
+
 private:
     uint8_t m_trigger_pin;
 	uint8_t m_echo_pin;
